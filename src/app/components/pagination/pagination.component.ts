43e88403a3f769bd232fccf9a29f47011a699fdc
Doomsdhg <80508769+Doomsdhg@users.noreply.github.com>
2023-08-13 22:39:38 +0500

    some performance improvements, optimization, small refactoring, formatting (#5)

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl } from '@angular/forms';
 import { combineLatest, first, map } from 'rxjs';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
@@ -7,20 +8,19 @@ import { StocksService } from 'src/app/services/stocks/stocks.service';
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  styleUrls: ['./pagination.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit {
+  private destroy_ref = inject(DestroyRef);
+
   public per_page_form = new FormControl<number>(this.local_storage_service.getStocksPerPage());
   public per_page_options = [10, 20, 30];
 
-  constructor(public readonly stocks_service: StocksService, private readonly local_storage_service: LocalStorageService) {
-    this.per_page_form.valueChanges.subscribe((value) => {
-      if (value) {
-        this.stocks_service.per_page$.next(value);
-        this.local_storage_service.setStocksPerPage(value);
-      }
-    })
-  }
+  constructor(
+    public readonly stocks_service: StocksService,
+    private readonly local_storage_service: LocalStorageService
+  ) {}
 
   public prev_button_disabled$ = this.stocks_service.page_number$.pipe(map((current_page) => current_page === 1));
   public next_button_disabled$ = combineLatest(
@@ -28,6 +28,17 @@ export class PaginationComponent {
   ).pipe(
     map(([stocks, per_page]) => stocks.length < per_page || !stocks.length)
   )
+
+  public ngOnInit(): void {
+    this.per_page_form.valueChanges
+      .pipe(takeUntilDestroyed(this.destroy_ref))
+      .subscribe((value) => {
+        if (value) {
+          this.stocks_service.per_page$.next(value);
+          this.local_storage_service.setStocksPerPage(value);
+        }
+      })
+  }
 
   public changePage(offset: number) {
     this.stocks_service.page_number$.pipe(first()).subscribe((current_page) => {
--- src/app/pages/stocks-page/stocks-page.component.ts
+++ src/app/pages/stocks-page/stocks-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StocksService } from 'src/app/services/stocks/stocks.service';
 
 @Component({
   selector: 'app-stocks-page',
   templateUrl: './stocks-page.component.html',
-  styleUrls: ['./stocks-page.component.scss']
+  styleUrls: ['./stocks-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StocksPageComponent {
   constructor(public readonly stocks_service: StocksService) {}
